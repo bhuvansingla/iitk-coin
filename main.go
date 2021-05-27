822089c241ebb66f20727614523191dff4a73428
Bhuvan Singla <bhuvansingla2000@gmail.com>
2021-05-27 19:24:19 +0530

    Add a basic http endpoint

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func foo(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("Works!")
+}
 
 func main() {
 	fmt.Println("Hello World")
+
+	http.HandleFunc("/", foo)
+
+	http.ListenAndServe(":8080", nil)
 }
