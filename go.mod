f062501abeb970312b63252e2e9e05db84105b90
Bhuvan Singla <bhuvansingla2000@gmail.com>
2021-05-28 21:24:45 +0530

    Add sqlite driver and addUser function

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/bhuvansingla/iitk-coin
 
 go 1.16
+
+require github.com/mattn/go-sqlite3 v1.14.7
--- main.go
+++ main.go
@@ -1,18 +1,59 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"net/http"
+
+	// "net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
-func foo(w http.ResponseWriter, req *http.Request) {
-	fmt.Println("Works!")
+func addUser(db *sql.DB, rollno string, name string) {
+	if userExists(db, rollno) {
+		fmt.Println("User", rollno, "Exists Already")
+		return
+	}
+	tx, err := db.Begin()
+	if err != nil {
+		fmt.Println("Error begining transaction")
+	}
+	stmt, err := db.Prepare("INSERT INTO User (rollno,name) VALUES (?,?)")
+	if err != nil {
+		fmt.Println("Error preparing statement")
+	}
+	_, err = stmt.Exec(rollno, name)
+	if err != nil {
+		fmt.Println("Error executing statement")
+	}
+	tx.Commit()
+	fmt.Println("User", rollno, "Added Succesfully")
 }
 
+func userExists(db *sql.DB, rollno string) bool {
+	row := db.QueryRow("SELECT rollno FROM User WHERE rollno=?", rollno)
+	scannedRow := ""
+	row.Scan(&scannedRow)
+	return scannedRow != ""
+}
+
+// func foo(w http.ResponseWriter, req *http.Request) {
+// 	fmt.Println("Works!")
+// }
+
 func main() {
-	fmt.Println("Hello World")
 
-	http.HandleFunc("/", foo)
+	db, errOpenDB := sql.Open("sqlite3", "coin.db")
 
-	http.ListenAndServe(":8080", nil)
+	if errOpenDB != nil {
+		fmt.Println("Error opening DB")
+	}
+
+	db.Exec("create table if not exists User (rollno text, name text)")
+
+	addUser(db, "180199", "Bhuvan Singla")
+	addUser(db, "180199", "Bhuvan Singla")
+
+	// http.HandleFunc("/", foo)
+	// http.ListenAndServe(":8080", nil)
 }
