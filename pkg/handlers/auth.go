b03ce7a61df715acdf71d7b0a4ddde0e0d61931b
Bhuvan Singla <bhuvansingla2000@gmail.com>
2021-06-09 04:36:23 +0530

    Add json response for auth requests

--- pkg/handlers/auth.go
+++ pkg/handlers/auth.go
@@ -2,45 +2,98 @@ package handlers
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 
 	"github.com/bhuvansingla/iitk-coin/pkg/auth"
 	"github.com/bhuvansingla/iitk-coin/pkg/user"
 
 	_ "github.com/mattn/go-sqlite3"
-	log "github.com/sirupsen/logrus"
 )
 
+type Response struct {
+	Success      bool   `json:"success"`
+	ErrorMessage string `json:"error"`
+}
+
+type SignupResponse struct {
+	Response
+}
+
+type LoginResponse struct {
+	Response
+	JwtToken string `json:"token"`
+}
+
 func Login(w http.ResponseWriter, req *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	if req.Method != "POST" {
+		var res Response
+		res.Success = false
+		res.ErrorMessage = "only POST method allowed"
+		json.NewEncoder(w).Encode(res)
+		return
+	}
+
+	var res LoginResponse
 	var u user.User
+
 	err := json.NewDecoder(req.Body).Decode(&u)
 	if err != nil {
-		fmt.Fprint(w, err.Error())
+		res.Success = false
+		res.ErrorMessage = err.Error()
+		json.NewEncoder(w).Encode(res)
 		return
 	}
+
 	token, err := auth.Login(&u)
 	if err != nil {
-		fmt.Fprint(w, err.Error())
+		res.Success = false
+		res.ErrorMessage = err.Error()
+		json.NewEncoder(w).Encode(res)
 		return
 	}
-	fmt.Fprint(w, token)
+
+	res.Success = true
+	res.JwtToken = token
+	json.NewEncoder(w).Encode(res)
+
 }
 
 func Signup(w http.ResponseWriter, req *http.Request) {
 
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	if req.Method != "POST" {
+		var res Response
+		res.Success = false
+		res.ErrorMessage = "only POST method allowed"
+		json.NewEncoder(w).Encode(res)
+		return
+	}
+
+	var res SignupResponse
 	var u user.User
+
 	err := json.NewDecoder(req.Body).Decode(&u)
 	if err != nil {
-		log.Error(err.Error())
-		fmt.Fprint(w, err.Error())
+		res.Success = false
+		res.ErrorMessage = err.Error()
+		json.NewEncoder(w).Encode(res)
 		return
 	}
+
 	err = auth.Signup(&u)
 	if err != nil {
-		log.Error(err.Error())
-		fmt.Fprint(w, err.Error())
+		res.Success = false
+		res.ErrorMessage = err.Error()
+		json.NewEncoder(w).Encode(res)
 		return
 	}
-	fmt.Fprint(w, "signed up successfully")
+
+	res.Success = true
+	json.NewEncoder(w).Encode(res)
 }
