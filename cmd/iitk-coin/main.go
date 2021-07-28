5f8808164d1fe647dfd23f06e77c92f74309b49c
Bhuvan Singla <bhuvansingla2000@gmail.com>
2021-07-29 01:13:28 +0530

    Add logs

--- cmd/iitk-coin/main.go
+++ cmd/iitk-coin/main.go
@@ -5,17 +5,20 @@ import (
 	"github.com/bhuvansingla/iitk-coin/database"
 	_ "github.com/bhuvansingla/iitk-coin/logger"
 	"github.com/bhuvansingla/iitk-coin/server"
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
 
 	err := database.Connect()
 	if err != nil {
+		log.Error("Error connecting to database: %s", err)
 		return
 	}
 
 	err = server.Start()
 	if err != nil {
+		log.Error("Error starting server: %s", err)
 		return
 	}
 
