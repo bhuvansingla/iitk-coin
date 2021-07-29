a351a05efbb54d2923e501eeb1b437214dfd2a59
Bhuvan Singla <bhuvansingla2000@gmail.com>
2021-07-29 21:35:54 +0530

    Remove unused function

--- account/wallet.go
+++ account/wallet.go
@@ -1,8 +1,6 @@
 package account
 
 import (
-	"database/sql"
-
 	"github.com/bhuvansingla/iitk-coin/database"
 )
 
@@ -13,12 +11,4 @@ func GetCoinBalanceByRollno(rollno string) (int, error) {
 		return 0, err
 	}
 	return coins, nil
-}
-
-func UpdateCoinBalanceByRollno(tx *sql.Tx, rollno string, coins int) error {
-	_, err := tx.Exec("UPDATE ACCOUNT SET coins=? WHERE rollno=?", coins, rollno)
-	if err != nil {
-		return err
-	}
-	return nil
 }
