17c638e510bedd7133ba837f8434638caa9d7edc
Bhuvan Singla <bhuvansingla2000@gmail.com>
2021-07-29 21:33:59 +0530

    Append logs to a file

--- logger/config.go
+++ logger/config.go
@@ -1,11 +1,23 @@
 package logger
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
 
 func init() {
-	logrus.SetReportCaller(true)
+	// logrus.SetReportCaller(true)
+
 	logrus.SetLevel(logrus.Level(viper.GetInt("LOGGER.LOG_LEVEL")))
+
+	f, err := os.OpenFile("iitkcoin.log", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
+	if err != nil {
+		fmt.Printf("error opening file: %v", err)
+	}
+
+	logrus.SetOutput(f)
+
 }
