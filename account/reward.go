d61aa3d088d57fe32e538945b3dede854b446e78
Harshit Raj <harshitr20@iitk.ac.in>
2021-10-20 12:50:47 +0530

    Add `REWARD_HISTORY`

--- account/reward.go
+++ account/reward.go
@@ -2,13 +2,14 @@ package account
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/bhuvansingla/iitk-coin/database"
 	"github.com/bhuvansingla/iitk-coin/errors"
 	"github.com/spf13/viper"
 )
 
-func AddCoins(rollno string, coins int) error {
+func AddCoins(rollno string, coins int, remarks string) error {
 
 	if err := validateCoinValue(coins); err != nil {
 		return err
@@ -22,34 +23,48 @@ func AddCoins(rollno string, coins int) error {
 
 	if err != nil {
 		tx.Rollback()
-		return err
-
+		return errors.NewHTTPError(err, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
 	}
 
 	limit := viper.GetInt("WALLET.UPPER_COIN_LIMIT")
 
 	res, err := tx.Exec("UPDATE ACCOUNT SET coins = coins + ? WHERE rollno=? AND coins + ? <= ?", coins, rollno, coins, limit)
+
 	if err != nil {
 		tx.Rollback()
-		return err
+		return errors.NewHTTPError(err, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
 	}
 
 	rowCnt, err := res.RowsAffected()
 	if err != nil {
 		tx.Rollback()
-		return err
+		return errors.NewHTTPError(err, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
 	}
 
 	if rowCnt == 0 {
 		tx.Rollback()
-		return errors.NewHTTPError(nil, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
+		return errors.NewHTTPError(nil, http.StatusBadRequest, "wallet upper limit reached")
+	}
+
+	stmt, err := tx.Prepare("INSERT INTO REWARD_HISTORY (rollno, coins, time, remarks) VALUES (?, ?, ?, ?)")
+
+	if err != nil {
+		tx.Rollback()
+		return errors.NewHTTPError(err, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
+	}
+
+	_, err = stmt.Exec(rollno, coins, time.Now(), remarks);
+	
+	if err != nil {
+		tx.Rollback()
+		return errors.NewHTTPError(err, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
 	}
 
 	err = tx.Commit()
 
 	if err != nil {
 		tx.Rollback()
-		return err
+		return errors.NewHTTPError(err, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
 	}
 
 	return nil
